de342293e2c7b304548ca1ba72fa73e45dc8265c
albinlukose777 <albin.lukose@itraveller.com>
2016-03-30 17:01:21 +0530

    Creatd Navigatytional drawer

--- MyApplication/app/src/main/java/albin/materialdesign/MainActivity.java
+++ MyApplication/app/src/main/java/albin/materialdesign/MainActivity.java
@@ -1,13 +1,95 @@
 package albin.materialdesign;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import albin.materialdesign.MyAdapter;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity {
+
+    String TITLES[] = {"Home","Events","Mail","Shop","Travel"};
+    int ICONS[] = {R.drawable.ic_action_name2,R.drawable.ic_action_name3,R.drawable.ic_action_name4,R.drawable.ic_action_name7,R.drawable.ic_action_name8};
+
+    String NAME = "Albin Lukose";
+    String EMAIL = "albinlukose777@gmail.com";
+    int PROFILE = R.drawable.anonymous;
+
+    RecyclerView mRecyclerView;
+    RecyclerView.Adapter mAdapter;
+    RecyclerView.LayoutManager mLayoutManager;
+    DrawerLayout Drawer;
+    ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.RecyclerView);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        mAdapter = new MyAdapter(TITLES,ICONS,NAME,EMAIL,PROFILE);
+
+        mRecyclerView.setAdapter(mAdapter);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        Drawer = (DrawerLayout) findViewById(R.id.DrawerLayout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,Drawer,toolbar,R.string.openDrawer,R.string.closeDrawer)
+        {
+            @Override
+
+            public void onDrawerOpened(View drawerView) {
+
+                super.onDrawerOpened(drawerView);
+
+            }
+
+            @Override
+
+            public void onDrawerClosed(View drawerView) {
+
+                super.onDrawerClosed(drawerView);
+
+            }
+
+        };
+        Drawer.setDrawerListener(mDrawerToggle);
+
+        mDrawerToggle.syncState();
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+     //   getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
